35ce1626a4cfe381d7e767f7f40a279b1e5686cb
sandramg <sangeorge04@gmail.com>
2024-02-07 12:47:55 +0530

    filemanipulation

---
+++ fileWordCount.java
@@ -0,0 +1,41 @@
+import java.io.*;
+class fileWordCount
+{
+    public static void main(String[] args)
+    {
+        int i,wordcount=0,linecount=0,charcount=0;
+        char ch;
+        try
+        {
+            FileInputStream fin=new FileInputStream("file1.txt");
+            do
+            {
+                i=fin.read();
+                ch=(char)i;
+                if(i!=-1)
+                {
+                    if(ch=='\n'|ch==' ')
+                    {
+                        wordcount++;
+                    }
+                    if(ch=='\n')
+                    {
+                        linecount++;
+                    }
+                    if(ch>='a'&ch<='z'|ch>='A'&ch<='Z')
+                    {
+                        charcount++;
+                    }
+                }
+            }while(i!=-1);
+            fin.close();
+        }
+        catch(Exception e)
+        {
+
+        }
+        System.out.println("The no.of lines is "+ ++linecount);
+        System.out.println("The no.of word is "+wordcount);
+        System.out.println("The no.of characters is "+charcount);
+    }
+}
