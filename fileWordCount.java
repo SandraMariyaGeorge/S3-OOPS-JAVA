edff7d953d1b63ef00f42350a6ca232490ce5823
sandramg <sangeorge04@gmail.com>
2024-02-07 12:48:33 +0530

    file

--- fileWordCount.java
+++ fileWordCount.java
@@ -1,41 +1,32 @@
 import java.io.*;
-class fileWordCount
-{
-    public static void main(String[] args)
-    {
-        int i,wordcount=0,linecount=0,charcount=0;
+
+class fileWordCount {
+    public static void main(String[] args) {
+        int i, wordcount = 0, linecount = 0, charcount = 0;
         char ch;
-        try
-        {
-            FileInputStream fin=new FileInputStream("file1.txt");
-            do
-            {
-                i=fin.read();
-                ch=(char)i;
-                if(i!=-1)
-                {
-                    if(ch=='\n'|ch==' ')
-                    {
+        try {
+            FileInputStream fin = new FileInputStream("file1.txt");
+            do {
+                i = fin.read();
+                ch = (char) i;
+                if (i != -1) {
+                    if (ch == '\n' | ch == ' ') {
                         wordcount++;
                     }
-                    if(ch=='\n')
-                    {
+                    if (ch == '\n') {
                         linecount++;
                     }
-                    if(ch>='a'&ch<='z'|ch>='A'&ch<='Z')
-                    {
+                    if (ch >= 'a' & ch <= 'z' | ch >= 'A' & ch <= 'Z') {
                         charcount++;
                     }
                 }
-            }while(i!=-1);
+            } while (i != -1);
             fin.close();
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
 
         }
-        System.out.println("The no.of lines is "+ ++linecount);
-        System.out.println("The no.of word is "+wordcount);
-        System.out.println("The no.of characters is "+charcount);
+        System.out.println("The no.of lines is " + ++linecount);
+        System.out.println("The no.of word is " + wordcount);
+        System.out.println("The no.of characters is " + charcount);
     }
 }
