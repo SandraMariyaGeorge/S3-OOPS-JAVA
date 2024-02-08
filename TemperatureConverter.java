f31c0c9b7781f23306cf7c0ceee7a7e2bb7c6d3c
sandramg <sangeorge04@gmail.com>
2024-02-08 18:26:10 +0530

    exception_handling

---
+++ TemperatureConverter.java
@@ -0,0 +1,24 @@
+
+//Create a class TemperatureConverter with a method convert To Fahrenheit that takes a temperature in Celsius as input 
+//and converts it to Fahrenheit. Handle the InputMismatchException 
+//if the user enters a non-numeric value when providing the temperature.F = (9/5)C + 32.
+import java.util.*;
+
+public class TemperatureConverter {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("enter the Celsius value");
+            double d = sc.nextDouble();
+            converttoFahrenheit(d);
+        } catch (InputMismatchException e) {
+            System.out.println("Please enter a non-numeric value");
+        }
+    }
+
+    static void converttoFahrenheit(double c) {
+        double f;
+        f = (9 / 5) * c + 32;
+        System.out.println("The value in fahrenheit is " + f);
+    }
+}
