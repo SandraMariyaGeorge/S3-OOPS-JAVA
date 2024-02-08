26ef4184e6823444b70ea01434f32dead1ae48aa
sandramg <sangeorge04@gmail.com>
2024-02-08 18:12:02 +0530

    package_qs

--- Calculator/Operations.java
+++ Calculator/Operations.java
@@ -2,27 +2,27 @@
 //multiplication subtraction and division of 2 numbers. Create another java file MyCalculator.java;
 //that accepts input from user and performs the operations in a Menu Driven
 package Calculator;
-public class Operations
-{
+
+public class Operations {
     int z;
-    public void add(int x,int y)
-    {
-        z=x+y;
-        System.out.println("Sum is "+z);
+
+    public void add(int x, int y) {
+        z = x + y;
+        System.out.println("Sum is " + z);
     }
-    public void sub(int x,int y)
-    {
-        z=x-y;
-        System.out.println("Difference is "+z);
+
+    public void sub(int x, int y) {
+        z = x - y;
+        System.out.println("Difference is " + z);
     }
-    public void multi(int x,int y)
-    {
-        z=x*y;
-        System.out.println("Product is "+z);
+
+    public void multi(int x, int y) {
+        z = x * y;
+        System.out.println("Product is " + z);
     }
-    public void div(int x,int y)
-    {
-        z=x/y;
-        System.out.println(x+"/"+y+" is "+z);
+
+    public void div(int x, int y) {
+        z = x / y;
+        System.out.println(x + "/" + y + " is " + z);
     }
 }
--- MyCalculator.java
+++ MyCalculator.java
@@ -7,30 +7,33 @@ import Calculator.*;
 
 class MyCalculator {
     public static void main(String[] args) {
-        int x,y,ch;
-        Operations op=new Operations();
-        Scanner sc=new Scanner(System.in);
-        do{
+        int x, y, ch;
+        Operations op = new Operations();
+        Scanner sc = new Scanner(System.in);
+        do {
             System.out.println("enter the 1st no");
-            x=sc.nextInt();
+            x = sc.nextInt();
             System.out.println("enter the 2nd no");
-            y=sc.nextInt();
+            y = sc.nextInt();
             System.out.println("enter your choice\n1-add\t2-sub\t3-multiply\t4-div\t5-exit");
-            ch=sc.nextInt();
-            switch(ch)
-            {
-                case 1:op.add(x,y);
-                break;
-                case 2:op.sub(x,y);
-                break;
-                case 3:op.multi(x,y);
-                break;
-                case 4:op.div(x,y);
-                break;
+            ch = sc.nextInt();
+            switch (ch) {
+                case 1:
+                    op.add(x, y);
+                    break;
+                case 2:
+                    op.sub(x, y);
+                    break;
+                case 3:
+                    op.multi(x, y);
+                    break;
+                case 4:
+                    op.div(x, y);
+                    break;
                 case 5:
-                break;
+                    break;
             }
 
-        }while(ch!=5);
+        } while (ch != 5);
     }
 }
